1a7e0d147e681cc8f792f83e01ecb879ebf0d000
Dylan <tettemerd@gmail.com>
2025-02-19 19:00:00 -0800

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -2,6 +2,8 @@
 #include <arpa/inet.h>
 #include <cstdlib>
 #include <cstring>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <netdb.h>
 #include <sstream>
@@ -15,10 +17,6 @@
 #include <vector>
 
 std::string construct_http_response(std::string &body) {
-  for (const auto &letter : body) {
-    std::cout << "LETTER: " << letter << '\n';
-  }
-
   std::string response =
       "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
   response.append(std::to_string(body.size()));
@@ -27,25 +25,45 @@ std::string construct_http_response(std::string &body) {
 
   return response;
 }
-
+std::string read_from_http_file(std::string_view file_path) {
+  std::ifstream file(file_path);
+  std::string file_content;
+  std::getline(file, file_content, '\0');
+  return file_content;
+}
 void handle_path(int client_fd, std::string &path,
-                 const std::unordered_map<std::string, std::string> &headers) {
+                 const std::unordered_map<std::string, std::string> &headers,
+                 std::string_view http_files_path) {
   if (path == "/") {
     std::string exists = "HTTP/1.1 200 OK\r\n\r\n";
     send(client_fd, exists.c_str(), exists.length(), 0);
   }
+
   if (path.find("echo") != std::string::npos) {
-    // Gets rid of /echo/ from the path
     std::string echo_string = path.substr(6, std::string::npos);
     std::string res = construct_http_response(echo_string);
     send(client_fd, res.c_str(), res.length(), 0);
   }
+
   if (path.find("user-agent") != std::string::npos) {
     std::string agent = headers.find("User-Agent")->second;
-    std::cout << agent.length() << '\n';
     std::string res = construct_http_response(agent);
     send(client_fd, res.c_str(), res.length(), 0);
-  } else {
+  }
+
+  if (path.find("files") != std::string::npos) {
+    std::string file_name = path.substr(path.find("files/"));
+    if (std::filesystem::exists(http_files_path.data() + file_name)) {
+      std::string http_file_content = read_from_http_file(http_files_path);
+      std::string res = construct_http_response(http_file_content);
+      send(client_fd, res.c_str(), res.length(), 0);
+    } else {
+      std::string no_exist = "HTTP/1.1 404 Not Found\r\n\r\n";
+      send(client_fd, no_exist.c_str(), no_exist.length(), 0);
+    }
+  }
+
+  else {
     std::string no_exist = "HTTP/1.1 404 Not Found\r\n\r\n";
     send(client_fd, no_exist.c_str(), no_exist.length(), 0);
   }
@@ -85,7 +103,6 @@ void handle_client_connection(int client_fd) {
         headers[key] = value;
       }
     }
-
     handle_path(client_fd, path, headers);
   }
 }
@@ -93,6 +110,11 @@ void handle_client_connection(int client_fd) {
 int main(int argc, char **argv) {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  std::string dir_path;
+  if (argv[1] == "--directory") {
+    dir_path = argv[2];
+  }
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
